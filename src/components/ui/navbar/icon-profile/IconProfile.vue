ec583d7c9bb41c71d2c6e89e5ad65ea01f331271
JSarco <jfe.sarco@gmail.com>
2021-10-30 20:44:03 +0200

    add profile

--- src/components/ui/navbar/icon-profile/IconProfile.vue
+++ src/components/ui/navbar/icon-profile/IconProfile.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="icon-container">
     <div class="icon-container__image">
-      <img alt="azul-blau" :src="IconJS" />
+      <!-- <img v-if="token && token === 'jfe.sarco@gmail.com'" alt="azul-blau" :src="IconJS" /> -->
+      <img alt="azul-blau" :src="IconNF" />
     </div>
   </div>
 </template>
 
 <script>
 import "./style.scss";
-import IconJS from "../../../../assets/avatar_js.png";
+// import IconJS from "../../../../assets/avatar_js.png";
+import IconNF from "../../../../assets/avatar_nf.png";
+// import { onMounted } from '@vue/runtime-core';
+// import { authPlugin } from "../../../../auth/index"
+
 
 export default {
   name: "IconProfile",
   components: {},
   setup() {
+
+    let token = null;
+
+    // onMounted(async () => {
+    //   token = await authPlugin.getIdTokenClaims();
+      
+    // });
+    
     return {
-      IconJS,
+        token,
+        IconNF
     };
   },
 };
