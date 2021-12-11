20c5cd3065b898c3f4e78f649019086afe8af02b
Jony Sarco <jfe.sarco@gmail.com>
2021-12-11 18:09:27 +0100

    adjust vue url and github actions

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,6 +1,5 @@
-
 export const auth0options = {
-    domain: process.env.VUE_APP_AUTH0_DOMAIN,
-    client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
-    redirect_uri: process.env.VUE_APP_AUTH_REDIRECT_URI,
-  };
+  domain: process.env.VUE_APP_AUTH0_DOMAIN,
+  client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
+  redirect_uri: process.env.VUE_APP_AUTH_REDIRECT_URI,
+};
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const URL = process.env.VUE_APP_BACKEND_URL;
+const URL = process.env.VUE_APP_BACKEND_URL;
 
 const instance = axios.create({
-  baseURL: "https://sheltered-cliffs-49107.herokuapp.com/",
+  baseURL: URL,
 });
 
 export default instance;
