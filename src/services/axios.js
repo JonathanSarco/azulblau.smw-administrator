f65bdd23887486beba0b65ac3576749924c37f7c
Jony Sarco <jfe.sarco@gmail.com>
2021-12-11 13:59:29 +0100

    fix

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = process.env.VUE_APP_BACKEND_URL;
+// const URL = process.env.VUE_APP_BACKEND_URL;
 
 const instance = axios.create({
   baseURL: "https://sheltered-cliffs-49107.herokuapp.com/",
